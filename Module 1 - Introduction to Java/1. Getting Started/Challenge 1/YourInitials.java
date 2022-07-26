ab6c06a38e32c2642ef8dfb6d5d968f565058171
Muntazir Rashid <77454113+muntazirrashid@users.noreply.github.com>
2022-07-26 19:33:35 +0100

    DONE

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
         //print initials here. 
+        System.out.println("M     M   R R R R");
+        System.out.println("M M M M   R      R");
+        System.out.println("M  M  M   R      R");
+        System.out.println("M     M   R R R R");
+        System.out.println("M     M   R   R");
+        System.out.println("M     M   R     R");
+        System.out.println("M     M   R      R");
     }
 }
