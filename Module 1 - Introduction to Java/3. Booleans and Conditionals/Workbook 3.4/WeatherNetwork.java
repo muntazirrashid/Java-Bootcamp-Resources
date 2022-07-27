841522408ed37850fc4d9844b69aa4a53fb3b8bf
Muntazir Rashid <muntazirrashid@Muntazirs-MacBook-Pro.local>
2022-07-27 11:05:33 +0100

    done

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
@@ -1,18 +1,24 @@
 public class WeatherNetwork {
     public static void main(String[] args) {
 
-        int temp = 25;  
+        int temp = 25;
 
         String forecast = "";
 
-        //IF - ELSE IF - ELSE STATEMENTS HERE!
+        // IF - ELSE IF - ELSE STATEMENTS HERE!
 
-        //if temp between -15 and -1: "The forecast is FREEZING! Stay home!"
-
-        //if temp between 0 and 10: "The forecast is Chilly. Wear a coat!";
-
-        //else: It's warm. go outside!
-
+        // if temp between -15 and -1: "The forecast is FREEZING! Stay home!"
+        if (temp >= -15 && temp <= -1) {
+            forecast = "The forecast is FREEZING! Stay home!";
+        }
+        // if temp between 0 and 10: "The forecast is Chilly. Wear a coat!";
+        else if (temp >= 0 && temp <= 10) {
+            forecast = "The forecast is Chilly. Wear a coat!";
+        }
+        // else: It's warm. go outside!
+        else {
+            forecast = "It's warm. go outside!";
+        }
         System.out.println(forecast);
     }
 }
